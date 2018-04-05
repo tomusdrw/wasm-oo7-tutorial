73e87ab2cf1f5159997761304290c0079be6ee16
Tomasz Drwięga <tomusdrw@gmail.com>
2018-04-05 12:03:32 +0200

    Contract ABI

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,62 @@
 #![no_std]
+#![feature(alloc)]
+#![feature(proc_macro)]
 
+extern crate alloc;
+extern crate bigint;
 extern crate pwasm_ethereum;
 extern crate pwasm_std;
+extern crate pwasm_abi;
+extern crate pwasm_abi_derive;
+extern crate parity_hash;
 
-use pwasm_std::hash::H256;
+mod contract {
+    use super::*;
+    use pwasm_std::hash::H256;
+    use pwasm_abi_derive::eth_abi;
+    use alloc::Vec;
 
-const HELLO: &str = "hello world";
+    static BLOCK_HASH_KEY: H256 = H256([2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
 
-// 3/ This function will be used as a key 
-fn block_hash_key() -> H256 {
-    0.into()
+    // 9/ Our contract definition (external ABI)
+    #[eth_abi(Endpoint, Client)]
+    pub trait Contract {
+        /// The constructor
+        fn constructor(&mut self);
+
+        /// Memoized block hash
+        #[constant]
+        fn block_hash(&mut self) -> Vec<u8>;
+    }
+
+
+    pub struct Instance;
+    impl Contract for Instance {
+        // 6/ Memoize blockhash in the contstructo
+        fn constructor(&mut self) {
+            pwasm_ethereum::write(
+                &BLOCK_HASH_KEY,
+                &pwasm_ethereum::block_hash(pwasm_ethereum::block_number() - 1).0
+            )
+        }
+
+        // 3/ Return a blockhash using constant function.
+        fn block_hash(&mut self) -> Vec<u8> {
+            pwasm_ethereum::read(&BLOCK_HASH_KEY).to_vec()
+        }
+    }
 }
+
+use pwasm_abi::eth::EndpointInterface;
 
 #[no_mangle]
 pub fn deploy() {
-    // 4/ During deployment we save the block hash
-    pwasm_ethereum::write(
-        &block_hash_key(),
-        &pwasm_ethereum::block_hash(pwasm_ethereum::block_number()).0
-    )
+    let mut endpoint = contract::Endpoint::new(contract::Instance);
+    endpoint.dispatch_ctor(&pwasm_ethereum::input());
 }
 
 #[no_mangle]
 pub fn call() {
-    // 3/ And return it back whenever the contract is called
-    pwasm_ethereum::ret(
-        &pwasm_ethereum::read(&block_hash_key())
-    )
+    let mut endpoint = contract::Endpoint::new(contract::Instance);
+    pwasm_ethereum::ret(&endpoint.dispatch(&pwasm_ethereum::input()));
 }
