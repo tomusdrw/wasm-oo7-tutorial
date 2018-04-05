1980263a5c9de265fa8d090e929c5c6318877d4e
Tomasz Drwięga <tomusdrw@gmail.com>
2018-04-05 09:31:31 +0200

    Storage access.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,30 @@
-// WASM contracts don't use Rust standard library.
 #![no_std]
 
-// Import the pwasm_ethereum bindings.
 extern crate pwasm_ethereum;
+extern crate pwasm_std;
 
-// 3/ That function will be called during contract deployment (think: constructor)
-#[no_mangle]
-pub fn deploy() {
+use pwasm_std::hash::H256;
 
+const HELLO: &str = "hello world";
+
+// 3/ This function will be used as a key 
+fn block_hash_key() -> H256 {
+    0.into()
 }
 
-// 5/ That function is run whenever the contract is called.
+#[no_mangle]
+pub fn deploy() {
+    // 4/ During deployment we save the block hash
+    pwasm_ethereum::write(
+        &block_hash_key(),
+        &pwasm_ethereum::block_hash(pwasm_ethereum::block_number()).0
+    )
+}
+
 #[no_mangle]
 pub fn call() {
-    // pwasm_ethereum bindings allow us to return a value or read input data.
-    pwasm_ethereum::ret("hello world!".as_bytes())
+    // 3/ And return it back whenever the contract is called
+    pwasm_ethereum::ret(
+        &pwasm_ethereum::read(&block_hash_key())
+    )
 }
