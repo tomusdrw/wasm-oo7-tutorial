312550e24b2511fa6f71bc7587f8ba4f3b54bcc4
Tomasz Drwięga <tomusdrw@gmail.com>
2018-04-05 14:51:22 +0200

    interact with contract

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 import {Rspan} from 'oo7-react';
-import {bonds, formatBalance, formatBlockNumber} from 'oo7-parity';
+import {bonds, formatBalance, formatBlockNumber, bytesToHex} from 'oo7-parity';
+import contractAbi from './abi.json';
 
 class App extends Component {
+
+  // 4/ Create a contract object.
+  contract = bonds.makeContract(
+    '0x4C801a31573e6B726fA02D6d8958C77203694FE2',
+    contractAbi
+  )
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Parity Dapp</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* 7/ Display default account, it's balance and block number */}
         <p>
           <Rspan>{bonds.me}</Rspan> has <strong><Rspan>
             {bonds.balance(bonds.me).map(formatBalance)}
           </Rspan></strong> at <strong><Rspan>
               {bonds.height.map(formatBlockNumber)}
           </Rspan></strong>
+        </p>
+
+        {/* 5/ Display a block hash stored in the contract */}
+        <p>
+          Contract has been deployed at <strong>
+            <Rspan>{this.contract.block_hash().map(bytesToHex)}</Rspan>
+          </strong>
         </p>
       </div>
     );
