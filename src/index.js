4c42e51e9e264d97b9b2b7ea767942ffe1544245
Tomasz Drwięga <tomusdrw@gmail.com>
2018-04-05 12:43:02 +0200

    oo7

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Import oo7
+import {TimeBond} from 'oo7';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Let's try to create the first bond.
+let bond = new TimeBond;
+
+// 4/ We tie the bond with a DOM element.
+bond.tie(val => {
+  console.log('Got notification from a bond.');
+  document.querySelector('#oo7').textContent = val;
+})
